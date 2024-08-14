8174ba0e759a67e6c84c63229eb19ba45143f67a
nmkhoi2708 <bitcoinisthechampion@gmail.com>
2024-08-14 10:08:04 +0700

    Update Linear.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Linear.java
@@ -7,11 +7,12 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 public class Linear {
-    private DcMotorEx frontLinear;
-    private DcMotorEx backLinear;
+    private DcMotorEx leftMotor;
+    private DcMotorEx rightMotor;
     private DcMotorEx middleLinear;
-    private CRServo servo1;
-    private CRServo servo2;
+    private List<DcMotor> motors;
+    private Servo servo1;
+    private Servo servo2;
     private final HardwareMap hardwareMap;
 
     public Linear(OpMode opMode) {
@@ -19,44 +20,51 @@ public class Linear {
     }
 
     public void init() {
-        frontLinear = hardwareMap.get(DcMotorEx.class, "frontLinear");
-        backLinear = hardwareMap.get(DcMotorEx.class, "backLinear");
-        middleLinear = hardwareMap.get(DcMotorEx.class, "middleLinear");
-        servo1 = hardwareMap.get(CRServo.class, "servo1");
-        servo2 = hardwareMap.get(CRServo.class, "servo2");
+        leftMotor = hardwareMap.get(DcMotorEx.class, "leftMotor");
+        rightMotor = hardwareMap.get(DcMotorEx.class, "rightMotor");
+        middleHex = hardwareMap.get(DcMotorEx.class, "elevator") ;
+        servo1 = hardwareMap.get(Servo.class, "servo1");
+        servo2 = hardwareMap.get(Servo.class, "servo2");
 
-        servo1.setDirection(CRServo.Direction.REVERSE);
-        servo2.setDirection(CRServo.Direction.FORWARD);
+        motors = Arrays.asList(leftMotor, rightMotor, middleHex);
 
-        frontLinear.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-        backLinear.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-        middleLinear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        frontLinear.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        backLinear.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
-        middleLinear.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        servo1.setPosition(0);
+        servo2.setPosition(0);
     }
 
-    public void setFrontLinear(double pow) {
-        frontLinear.setPower(pow);
+    public void setMode(DcMotor.RunMode runMode) {
+        for (DcMotor motor : motors) {
+            motor.setMode(runMode);
+        }
     }
 
-    public void setBackLinear(double pow) {
-        backLinear.setPower(pow);
+    public void setZeroPowerBehavior(DcMotor.ZeroPowerBehavior zeroPowerBehavior) {
+        for (DcMotor motor : motors) {
+            motor.setZeroPowerBehavior(zeroPowerBehavior);
+        }
+    }
+   public void setLeftMotor(double pow) {
+        leftMotor.setPower(pow);
     }
 
-    public void setMiddleLinear(double pow) {
-        middleLinear.setPower(pow);
+    public void setRightMotor(double pow) {
+        rightMotor.setPower(pow);
     }
+
+    public void setElevator(double pow) { middleHex.setPower(pow);}
 
     public void setAllLinear(double pow) {
-        setBackLinear(pow);
-        setFrontLinear(pow);
-        setMiddleLinear(pow);
+        setRightMotor(pow);
+        setLeftMotor(pow);
     }
 
-    public void setLinearServo(double pow) {
-        servo2.setPower(pow);
-        servo1.setPower(pow);
+    public void setLinearServo() {
+        servo1.setPosition(0.27);
+        servo2.setPosition(0.27);
     }
 }
